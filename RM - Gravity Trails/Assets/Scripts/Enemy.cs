bee3ac560fb9dd0a2b73c51fde180e96fe9a4929
Hayden <104737991+progamebuilder67@users.noreply.github.com>
2024-04-16 18:59:38 -0400

    4/16
    on step 49/ page 37

---
+++ RM - Gravity Trails/Assets/Scripts/Enemy.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int maximumXPosition;
+    public int minimumXPosition;
+    public float yForce;
+    public float xForce;
+    public float xDirection;
+    public float yDirection;
+    private Rigidbody2D enemyRigidBody;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyRigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            Vector2 jumpForce = new Vector2(xForce * xDirection, yForce);
+            enemyRigidBody.AddForce(jumpForce);
+        }
+        if (collision.gameObject.tag == "Wall")
+        {
+            Debug.Log("hit a wall");
+            float temp = xDirection * -1;
+            xDirection = temp;
+            Vector2 jumpForce = new Vector2(xForce * xDirection, yForce);
+            enemyRigidBody.AddForce(jumpForce);
+        }
+    }
+    private void FixedUpdate()
+    {
+        /*
+        if (transform.position.x <= minimumXPosition)
+        {
+            xDirection = 1;
+            enemyRigidBody.AddForce(Vector2.right * xForce);
+        }
+        if (transform.position.x >= maximumXPosition)
+        {
+            xDirection = -1;
+            enemyRigidBody.AddForce(Vector2.left * xForce);
+        }
+        */
+    }
+}
