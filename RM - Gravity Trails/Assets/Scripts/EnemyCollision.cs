1af97f071f583a42ec089a969c7e6c28b3cf46cc
CNMarkham <markhamonca@codeninjas.com>
2024-04-21 15:47:29 -0400

    4/21
    on step 66 :DDD

--- RM - Gravity Trails/Assets/Scripts/EnemyCollision.cs
+++ RM - Gravity Trails/Assets/Scripts/EnemyCollision.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class EnemyCollision : MonoBehaviour
 {
     // Start is called before the first frame update
@@ -9,10 +9,13 @@ public class EnemyCollision : MonoBehaviour
     {
         
     }
-
+    
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        if (collision.gameObject.tag == "Enemy")
+        {
+            SceneManager.LoadScene(0);
+        }
     }
     // Update is called once per frame
     void Update()
