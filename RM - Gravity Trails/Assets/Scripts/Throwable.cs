551010b2767af49eefe0a2543d38f4196a9ed539
Bye <markhamonca@codeninjas.com>
2024-04-23 19:00:26 -0400

    4/23
    on step 82 page 54

--- RM - Gravity Trails/Assets/Scripts/Throwable.cs
+++ RM - Gravity Trails/Assets/Scripts/Throwable.cs
@@ -6,19 +6,35 @@ public class Throwable : MonoBehaviour
 {
     public GameObject objectThrown;
     public Vector3 offset;
-    //offset = transform.localScale.x * new Vector3(1,0,0);
-    // Start is called before the first frame update
+    public int throwableCounter;
     void Start()
     {
         
     }
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Collectable")
+        {
+            throwableCounter++;
+            Destroy(collision.gameObject);
+        }
+    }
+
+
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
+        if (Input.GetButtonDown("Fire1") && throwableCounter > 0 )
         {
-            Instantiate(objectThrown, transform.position, transform.rotation);
+            throwableCounter--;
+            offset = transform.localScale.x * new Vector3(1, 0, 0);
+            Vector3 throwablePosition = transform.position + offset;
+            Instantiate(objectThrown, throwablePosition, transform.rotation);
+
         }
+
+       
+        //Instantiate(objectThrown, throwablePosition, transform.rotation);
     }
 }
